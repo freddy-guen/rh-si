7c6efc72ce2215c3b9202f1645f536f45cc1c57a
Freddy GUENENGAFO <guenengafo-biroboy.f@mipih.fr>
2025-02-25 13:34:49 +0100

    Init project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { EmployeListComponent } from './component/employe-list/employe-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path : 'employe',
+    component : EmployeListComponent
+  }
+];
---
+++ src/app/model/employe.ts
@@ -0,0 +1,9 @@
+export interface Employe {
+    id : number;
+    nom : string;
+    prenom : string;
+    dateNaissance : Date;
+    role : string;
+    dateEntree : Date;
+    salaire : number;
+}
